9df12ab5f0b5531b57ba3dd5710bf94ab423906c
dbonafin <demianbonafina@gmail.com>
2022-06-27 17:12:46 +0200

    milestone 3 and bonus 1 done

--- js/script.js
+++ js/script.js
@@ -3,25 +3,24 @@
 // - text, una stringa che indica il testo del todo
 // - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
 
-// MILESTONE 1 - Html Ln 22 -
+// MILESTONE 1 - Html Ln 30 -
 // Stampare all'interno di una lista, un item per ogni todo.
 // Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 
-// MILESTONE 2 - Html Ln 28 -
+// MILESTONE 2 - Html Ln 35 -
 // Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 
-// MILESTONE 3
+// MILESTONE 3 - Js Ln 47 -
 // Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 
+// Bonus 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista  - Html Ln 20 -
+// Bonus 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa) 
 
 var app = new Vue(
    {
     el: "#root",
     data: {
-        newToDo: {
-            text: "",
-            done: false
-        },
+        newToDoText: "",
         listToDo: [
             {
                 text: "Andare in palestra",
@@ -46,11 +45,18 @@ var app = new Vue(
             this.listToDo.splice(index, 1);
         },
         addItem() {
-            if(this.newToDo.text.length != 0) {
-                this.listToDo.push(this.newToDo);
-                this.newToDo.text = ""; 
+            if(this.newToDoText.length != 0) {
+                const newToDo =   {
+                    text: this.newToDoText,
+                    done: false
+                };
+                this.listToDo.push(newToDo);
+                this.newToDoText = ""; 
             }
         },
+        // doneToggle(index) {
+        //     this.listToDo(index)
+        // }
     }
    }
 );
