629928c96cee5ecdce3004bec49e0d88ad40983a
dbonafin <demianbonafina@gmail.com>
2022-06-27 18:02:03 +0200

    bonus 2 done

--- js/script.js
+++ js/script.js
@@ -14,49 +14,46 @@
 // Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 
 // Bonus 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista  - Html Ln 20 -
-// Bonus 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa) 
+// Bonus 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa) - Html Ln 30 -
 
 var app = new Vue(
    {
-    el: "#root",
-    data: {
-        newToDoText: "",
-        listToDo: [
-            {
-                text: "Andare in palestra",
-                done: false
-            },
-            {
-                text: "Fare la spesa",
-                done: false
-            },
-            {
-                text:"Chiamare il dottore",
-                done: true
-            },
-            {
-                text:"Andare dal barbiere",
-                done: false
-            },
-        ]
-    },
-    methods: {
-        removeItem(index) {
-            this.listToDo.splice(index, 1);
-        },
-        addItem() {
-            if(this.newToDoText.length != 0) {
-                const newToDo =   {
-                    text: this.newToDoText,
+        el: "#root",
+        data: {
+            newToDoText: "",
+            listToDo: [
+                {
+                    text: "Andare in palestra",
                     done: false
-                };
-                this.listToDo.push(newToDo);
-                this.newToDoText = ""; 
-            }
+                },
+                {
+                    text: "Fare la spesa",
+                    done: false
+                },
+                {
+                    text:"Chiamare il dottore",
+                    done: true
+                },
+                {
+                    text:"Andare dal barbiere",
+                    done: false
+                },
+            ]
         },
-        // doneToggle(index) {
-        //     this.listToDo(index)
-        // }
+        methods: {
+            removeItem(index) {
+                this.listToDo.splice(index, 1);
+            },
+            addItem() {
+                if(this.newToDoText.length != 0) {
+                    const newToDo =   {
+                        text: this.newToDoText,
+                        done: false
+                    };
+                    this.listToDo.push(newToDo);
+                    this.newToDoText = ""; 
+                }
+            },
+        }
     }
-   }
 );
